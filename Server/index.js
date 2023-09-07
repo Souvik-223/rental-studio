75d41f52875b7c5a9d0ae6093e455c7382894da3
Souvik chakraborty <22souvik22@gmail.com>
2023-09-07 11:37:08 +0530

    Server setup

--- Server/index.js
+++ Server/index.js
@@ -1,6 +1,11 @@
 const express = require('express')
 const app = express()
 
+app.use(cors({
+    Credential: true,
+    origin:'http://127.0.0.1:5173/',
+}));
+
 app.get('/test',(req,res)=>{
     res.json('test ok');
 });
