cfdb117fc8d80b8590c6fa222085ef37088637de
Souvik chakraborty <22souvik22@gmail.com>
2023-09-19 14:27:11 +0530

    fiX Client

--- Client/src/Components/Placedetailscomponents/Placedetails.jsx
+++ Client/src/Components/Placedetailscomponents/Placedetails.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
 import axios from 'axios'
 import Bookingoperation from "./Bookingoperation";
-import Upperdetails from "./subdetailscomponenets/Upperdetails";
+import Upperdetails from './subdetailscomponenets/Upperdetails'
 
 export default function Placedetails() {
     const { id } = useParams()
